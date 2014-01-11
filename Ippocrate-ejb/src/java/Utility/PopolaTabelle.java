11bbe1bc4b570720798018880c958e8615d0dca7
Tobia Giani <gianitobia@gmail.com>
2014-01-11 16:56:39 +0100

    Risolto merge di vari file Merge origin/master
    Conflicts:
    	Ippocrate-ejb/src/java/Controller/GestoreLogin.java
    	Ippocrate-ejb/src/java/Controller/GestoreLoginLocal.java
    	Ippocrate-war/src/java/web/LoginServlet.java
    	Ippocrate-war/web/errore.jsp
    	Ippocrate-war/web/home-paziente.jsp
    	Ippocrate-war/web/index.html
    	Ippocrate-war/web/login.jsp

--- Ippocrate-ejb/src/java/Utility/PopolaTabelle.java
+++ Ippocrate-ejb/src/java/Utility/PopolaTabelle.java
@@ -1,13 +1,15 @@
 package Utility;
 
+import Controller.GestoreInserimentoDatiLocal;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Date;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
 /*
  * To change this template, choose Tools | Templates
@@ -18,78 +20,74 @@ import java.util.Scanner;
  * @author: Varesano
  */
 public class PopolaTabelle {
+    GestoreInserimentoDatiLocal gestoreInserimentoDati = lookupGestoreInserimentoDatiLocal();
+    
+//    public static void popolaCartellaClinica()
+//            throws SQLException, FileNotFoundException, IOException {
+//
+//        Connection c = DriverManager.getConnection(url, user, pwd);
+//        Statement st = c.createStatement();
+//        Scanner s = new Scanner(new File("src/java/Utility/CartellaClinica.txt"));
+//        String[] valori;
+//        while (s.hasNextLine()) {
+//            valori = s.nextLine().split(",");
+//            st.executeUpdate("INSERT INTO CARTELLACLINICA(ID, ANAMNESI) "
+//                    + "VALUES('" + valori[0] + "','" + valori[1] + "')");
+//        }
+//        st.close();
+//        c.close();
+//    }
 
-    public static void popolaCartellaClinica(String url, String user, String pwd)
-            throws SQLException, FileNotFoundException, IOException {
+    public void popolaPaziente() throws FileNotFoundException {
 
-        Connection c = DriverManager.getConnection(url, user, pwd);
-        Statement st = c.createStatement();
-        Scanner s = new Scanner(new File("src/java/Utility/CartellaClinica.txt"));
-        String[] valori;
-        while (s.hasNextLine()) {
-            valori = s.nextLine().split(",");
-            st.executeUpdate("INSERT INTO CARTELLACLINICA(ID, ANAMNESI) "
-                    + "VALUES('" + valori[0] + "','" + valori[1] + "')");
-        }
-        st.close();
-        c.close();
-    }
-
-    public static void popolaPaziente(String url, String user, String pwd)
-            throws SQLException, FileNotFoundException, IOException {
-
-        Connection c = DriverManager.getConnection(url, user, pwd);
-        Statement st = c.createStatement();
         Scanner s = new Scanner(new File("src/java/Utility/Paziente.txt"));
         String[] valori;
         while (s.hasNextLine()) {
             valori = s.nextLine().split(",");
-            st.executeUpdate("INSERT INTO PAZIENTE(ID, NOME, COGNOME, PASSWORD"
-                    + ", CF, SESSO, INDIRIZZO, DATA_NASCITA, LUOGO_NASCITA, CARTELLA_CLINICA_ID) "
-                    + "VALUES('" + valori[0] + "','" + valori[1] + "','" + valori[2] + "','" + valori[3]
-                    + "','" + valori[4] + "','" + valori[5] + "','" + valori[6]
-                    + "','" + valori[7] + "','" + valori[8] + "','" + valori[9] + "')");
+            
+            gestoreInserimentoDati.addPaziente(valori[1], valori[2], valori[4], valori[3], valori[5], valori[6], new Date(valori[7]), valori[8]);
+//            st.executeUpdate("INSERT INTO PAZIENTE(ID, NOME, COGNOME, PASSWORD"
+//                    + ", CF, SESSO, INDIRIZZO, DATA_NASCITA, LUOGO_NASCITA, CARTELLA_CLINICA_ID) "
+//                    + "VALUES('" + valori[0] + "','" + valori[1] + "','" + valori[2] + "','" + valori[3]
+//                    + "','" + valori[4] + "','" + valori[5] + "','" + valori[6]
+//                    + "','" + valori[7] + "','" + valori[8] + "','" + valori[9] + "')");
         }
-        st.close();
-        c.close();
     }
 
-    public static void popolaAgenda(String url, String user, String pwd)
-            throws SQLException, FileNotFoundException, IOException {
-
-        Connection c = DriverManager.getConnection(url, user, pwd);
-        Statement st = c.createStatement();
-        Scanner s = new Scanner(new File("src/java/Utility/Agenda.txt"));
-        String[] valori;
-        while (s.hasNextLine()) {
-            valori = s.nextLine().split(",");
-            st.executeUpdate("INSERT INTO AGENDA(ID, CLIENT_ID, DEVELOPER_KEY, ID_CALENDARIO,"
-                    + " NOME_UTENTE, PASSWORD, SECRET_KEY) "
-                    + "VALUES('" + valori[0] + "','" + valori[1] + "','" + valori[2] + "','" + valori[3]
-                    + "','" + valori[4] + "','" + valori[5] + "','" + valori[6] + "')");
-        }
-        st.close();
-        c.close();
-    }
+//    public static void popolaAgenda()
+//            throws SQLException, FileNotFoundException, IOException {
+//
+//        Scanner s = new Scanner(new File("src/java/Utility/Agenda.txt"));
+//        String[] valori;
+//        while (s.hasNextLine()) {
+//            valori = s.nextLine().split(",");
+//            Agenda a = new Agenda();
+//            a.setId(Long.parseLong(valori[0]));
+//            /*st.executeUpdate("INSERT INTO AGENDA(ID, CLIENT_ID, DEVELOPER_KEY, ID_CALENDARIO,"
+//                    + " NOME_UTENTE, PASSWORD, SECRET_KEY) "
+//                    + "VALUES('" + valori[0] + "','" + valori[1] + "','" + valori[2] + "','" + valori[3]
+//                    + "','" + valori[4] + "','" + valori[5] + "','" + valori[6] + "')");*/
+//           agendaFacade.create(a);
+//        }
+//    }
 
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/ippocrate?zeroDateTimeBehavior=convertToNull";
-        /* Inserisci il tuo user e la tua password */
-        String user = MySqlAuth.user;
-        String pwd = MySqlAuth.pwd;
-
+        PopolaTabelle pt = new PopolaTabelle();
         try {
-            //Class.forName("com.mysql.jdbc.Driver").newInstance();
-
-            //Establish connection using DriverManager
-            //Connection conn = DriverManager.getConnection(url, user, pwd);
-            popolaCartellaClinica(url, user, pwd);
-            popolaPaziente(url, user, pwd);
-            popolaAgenda(url, user, pwd);
-
-            //conn.close();
-        } catch (Exception e) {
-            System.out.println(e);
+            pt.popolaPaziente();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(PopolaTabelle.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+
+    private GestoreInserimentoDatiLocal lookupGestoreInserimentoDatiLocal() {
+        try {
+            Context c = new InitialContext();
+            return (GestoreInserimentoDatiLocal) c.lookup("java:global/Ippocrate/Ippocrate-ejb/GestoreInserimentoDati!Controller.GestoreInserimentoDatiLocal");
+        } catch (NamingException ne) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
+            throw new RuntimeException(ne);
+        }
+    }
+
 }
